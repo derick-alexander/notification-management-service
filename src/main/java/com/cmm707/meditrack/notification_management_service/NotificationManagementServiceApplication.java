2f12888b3a8a964e4c2008b7b6f4cda5207098a0
derick-alexander <derickawsalexander@gmail.com>
2024-12-18 01:08:50 +0530

    first commit

---
+++ src/main/java/com/cmm707/meditrack/notification_management_service/NotificationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.notification_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationManagementServiceApplication.class, args);
+	}
+
+}
